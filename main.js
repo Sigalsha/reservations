522762230c35461af3d14b94b731b9f5d4dc37a6
Sigal Shalit Manor <sigal.shalit.manor@gmail.com>
2018-07-28 21:48:53 +0300

    complete individual exercise

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//reservations ex.
+
 var reservations = {
   'Bob': { claimed: false },
   'Ted': { claimed: true }
@@ -5,8 +7,40 @@ var reservations = {
 
 var name = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
+
   // write your code here!
+
+let findReservationName = function(reservationName) {
+    reservationName = reservationName.toLowerCase();
+    for (key in reservations) {
+       let keyLowerCase = key.toLowerCase();
+        if (keyLowerCase === reservationName) {
+            return reservations[key];
+        }
+    }
+    return null;
 }
 
-claimReservation();
+
+let addReservation = function(reservationName) {
+    alert("there is nothing under your name");
+    reservations[reservationName] = { claimed: true };
+    alert("we added your new reservation");
+}   
+
+
+var claimReservation = function(reservationName) {
+    let reservationInfo = findReservationName(reservationName);
+    if (reservationInfo === null) {
+        addReservation(reservationName);
+    } else if (reservationInfo.claimed === true) {
+        alert("welcome!");
+    } else if (reservationInfo.claimed === false) {
+        alert("your reservation exist, but was already claimed");
+    }
+}
+
+claimReservation(name);
+
+ 
+
